782d9b9d4876457dfe19ad5a4b5406bf54ce09eb
jhammarstedt <johan.hammarstedt@gmail.com>
2021-04-06 19:27:58 +0000

    deploy fix

--- containers/train/deploy.sh
+++ containers/train/deploy.sh
@@ -7,17 +7,17 @@ MODEL_VERSION=1
 MODEL_DIR= "projects/ml-pipeline-309409/models/"
 #create new model
 # Get the name from 
-modelname= $(gcloud ai-platform models list --region global| grep -w "$MODEL_NAME") #might give me trouble
-echo $modelname
+#modelname= $(gcloud ai-platform models list --region global| grep -w "$MODEL_NAME") #might give me trouble
+#echo $modelname
 
 #check if the string is empty, if so we create a new model
-if [-z "$modelname"]; then
-	echo "Creating model $MODEL_NAME"
+#if [-z "$modelname"]; then
+echo "Creating model $MODEL_NAME"
 	
-	gcloud ai-platform models create ${MODEL_NAME} --regions $REGION
-else
-	echo "The model $MODEL_NAME already exists"
-fi
+gcloud ai-platform models create ${MODEL_NAME} --regions $REGION
+#else
+#	echo "The model $MODEL_NAME already exists"
+#fi
 
 echo "Create version $MODEL_VERSION from $MODEL_DIR"
 gcloud ai-platform versions create ${MODEL_VERSION}\
