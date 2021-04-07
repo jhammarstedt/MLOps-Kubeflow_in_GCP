d9f4793732341cacc34d48d000228d9b527b4700
jhammarstedt <52280124+jhammarstedt@users.noreply.github.com>
2021-04-07 16:33:49 +0200

    Update set_auth.sh

--- set_auth.sh
+++ set_auth.sh
@@ -1,45 +1,31 @@
 #!/bin/bash
-# Copyright 2020 Google LLC
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#      http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
 
-if [ "$#" -ne 4 ]; then
+
+if [ "$#" -ne 3 ]; then
     echo "Usage: ./set_auth.sh service-account-name zone cluster namespace"
-    echo "     Get these values by visiting https://console.cloud.google.com/ai-platform/pipelines/clusters"
-    echo "  eg:  ./set_auth.sh kfpdemo europe-north1-c cluster-1 default"
+    echo " 
+    echo "     Create a new name and get the other values at https://console.cloud.google.com/ai-platform/pipelines/clusters"
+    echo "  eg:  ./set_auth.sh ml-demo europe-north1-c cluster-1 default"
     exit
 fi
 
-PROJECT_ID=$(gcloud config get-value project)
-SA_NAME=$1
+PROJECT_ID=$(gcloud config config-helper --format "value(configuration.properties.core.project)")
+SA_NAME=$1 
 ZONE=$2
 CLUSTER=$3
-NAMESPACE=$4
+NAMESPACE= "default"
 
 
-# See: https://github.com/kubeflow/pipelines/blob/master/manifests/gcp_marketplace/guide.md#gcp-service-account-credentials
 
 gcloud container clusters get-credentials "$CLUSTER" --zone "$ZONE" --project "$PROJECT_ID"
 
-
-# Create service account
+#create the service account
 gcloud iam service-accounts create $SA_NAME \
        --display-name $SA_NAME --project "$PROJECT_ID"
 
 # Grant permissions to the service account by binding roles
 # roles/editor is needed to launch a CAIP Notebook.
 # The others (storage, bigquery, ml, dataflow) are pretty common for GCP ML pipelines
-# That said, "admin" is a bit of an overkill; you might want to provide narrower roles for your users
 for ROLE in roles/editor roles/storage.admin roles/bigquery.admin roles/ml.admin roles/dataflow.admin roles/pubsub.admin; do
   gcloud projects add-iam-policy-binding $PROJECT_ID \
     --member=serviceAccount:$SA_NAME@$PROJECT_ID.iam.gserviceaccount.com \
