da59ecd0f3e7cbc6947e3bb58d31447bc47f3b51
jhammarstedt <johan.hammarstedt@gmail.com>
2021-05-04 19:26:39 +0000

    docker changes

--- containers/train/setup_model.sh
+++ containers/train/setup_model.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
+echo "Running traning model.py"
+python train_test.py
+
+
 #This file creates a local package with all our pytorch requirements and then copies it to our bucket
-print('SETTING UP MODEL')
+echo "SETTING UP MODEL"
 python setup.py sdist
 DIST_PACKAGE_BUCKET='gs://ml-pipeline-309409_bucket/models/Test_model-0.1.tar.gz'
 LOCAL_PACKAGE='dist/Test_model-0.1.tar.gz'
