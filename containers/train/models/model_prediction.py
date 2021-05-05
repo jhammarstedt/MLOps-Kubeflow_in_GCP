652fbfdc89e50a31a479b9c0ebe6b12ace75c6d7
jhammarstedt <johan.hammarstedt@gmail.com>
2021-05-05 18:49:41 +0200

    t

--- containers/train/models/model_prediction.py
+++ containers/train/models/model_prediction.py
@@ -4,19 +4,19 @@ import pandas as pd
 from google.cloud import storage
 import torch
 class PyTorchIrisClassifier(object):
-   def __init__(self, model):
-       self._model = model
-       self.class_vocab = ['setosa', 'versicolor', 'virginica']
-   @classmethod
-  
-   def from_path(cls, model_dir):
-       model_file = os.path.join(model_dir,'model.pth')
-       model = torch.load(model_file)
-       return cls(model)
-      
-   def predict(self, instances, **kwargs):
-       data = pd.DataFrame(instances).as_matrix()
-       inputs = torch.Tensor(data)
-       outputs = self._model(inputs)
-       _ , predicted = torch.max(outputs, 1)
-       return [self.class_vocab[class_index] for class_index in predicted]
+    def __init__(self, model):
+        self._model = model
+        self.class_vocab = ['setosa', 'versicolor', 'virginica']
+    
+    @classmethod
+    def from_path(cls, model_dir):
+        model_file = os.path.join(model_dir,'model.pth')
+        model = torch.load(model_file)
+        return cls(model)
+        
+    def predict(self, instances, **kwargs):
+        data = pd.DataFrame(instances).as_matrix()
+        inputs = torch.Tensor(data)
+        outputs = self._model(inputs)
+        _ , predicted = torch.max(outputs, 1)
+        return [self.class_vocab[class_index] for class_index in predicted]
