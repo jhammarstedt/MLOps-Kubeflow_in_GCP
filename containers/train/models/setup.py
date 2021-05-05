1f9523b7d6777c4a7aac4a097c948211f40077a0
jhammarstedt <johan.hammarstedt@gmail.com>
2021-05-05 17:27:18 +0000

    deployment working

--- containers/train/models/setup.py
+++ containers/train/models/setup.py
@@ -7,5 +7,5 @@ setup(
    name="Test_model",
    version="0.1",
    scripts=["model_prediction.py"],
-   #install_requires=REQUIRED_PACKAGES
+   install_requires=install_requires=['torch @ https://download.pytorch.org/whl/cpu/torch-1.8.1-cp37-cp37m-linux_x86_64.whl']
 )
