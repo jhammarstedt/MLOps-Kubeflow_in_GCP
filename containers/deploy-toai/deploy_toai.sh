33e99d319dea1f11243702c5330ed13d8b419461
jhammarstedt <johan.hammarstedt@gmail.com>
2021-05-05 16:42:01 +0000

    working dep

--- containers/deploy-toai/deploy_toai.sh
+++ containers/deploy-toai/deploy_toai.sh
@@ -15,7 +15,7 @@
 MODEL_NAME='test_iris'
 MODEL_VERSION='v1'
 RUNTIME_VERSION='1.14' 
-MODEL_CLASS='model.PyTorchIrisClassifier'
+MODEL_CLASS='model_prediction.PyTorchIrisClassifier'
 PYTORCH_PACKAGE='gs://ml-pipeline-309409_bucket/packages/torch-1.8.1+cpu-cp37-cp37m-linux_x86_64.whl'
 DIST_PACKAGE='gs://ml-pipeline-309409_bucket/models/Test_model-0.1.tar.gz'
 BUCKET_NAME='ml-pipeline-309409_bucket'
@@ -30,12 +30,13 @@ gcloud alpha ai-platform models create ${MODEL_NAME} \
        	--enable-logging \
 	--enable-console-logging
 
-gcloud beta ai-platform versions create ${MODEL_VERSION} --model=${MODEL_NAME} \
+gcloud beta ai-platform versions create ${MODEL_VERSION} \
+    --model=${MODEL_NAME} \
     --origin=gs://${BUCKET_NAME}/${GCS_MODEL_DIR}/ \
     --python-version=3.5 \
     --region=${REGION} \
     --machine-type='mls1-c4-m4' \
     --runtime-version=${RUNTIME_VERSION} \
-    --package-uris=${DIST_PACKAGE},${PYTORCH_PACKAGE} \
+    --package-uris=${DIST_PACKAGE} \
     --prediction-class=${MODEL_CLASS}
     
