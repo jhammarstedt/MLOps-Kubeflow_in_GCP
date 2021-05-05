b0e98fb260363f58e6edb648a7946b39211426e7
jhammarstedt <johan.hammarstedt@gmail.com>
2021-05-05 19:35:51 +0200

    working setup file

--- containers/train/models/setup.py
+++ containers/train/models/setup.py
@@ -1,11 +1,12 @@
 
 from setuptools import setup
  
-REQUIRED_PACKAGES = []
+#REQUIRED_PACKAGES = []
  
 setup(
    name="Test_model",
    version="0.1",
    scripts=["model_prediction.py"],
-   #install_requires=REQUIRED_PACKAGES
+   include_package_data=True,
+   install_requires=['torch @ https://download.pytorch.org/whl/cpu/torch-0.4.1-cp35-cp35m-linux_x86_64.whl']
 )
