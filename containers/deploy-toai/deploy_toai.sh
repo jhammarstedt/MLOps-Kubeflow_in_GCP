aad5b0c566328dc5ee3dab9051d7f569beb303df
Matej Sestak <matej.sestak@hotmail.com>
2021-05-06 09:54:40 +0200

    added project name

--- containers/deploy-toai/deploy_toai.sh
+++ containers/deploy-toai/deploy_toai.sh
@@ -5,16 +5,16 @@
 # Creating a version resrouce
 # origin: Path to GCS directory where the model file is stored.
 # python-version: The python version your code is running onto.
-# runtime-version: AI platform runtime version. 
+# runtime-version: AI platform runtime version.
 # machine-type: Machine type is defined as per the requirement of resources for the model to be deployed.
-#    Since we are using our own custom prediction routine, there are two machine types available, 
+#    Since we are using our own custom prediction routine, there are two machine types available,
 #    Mls1-c4-m4, mls1-c4-m2.
 # package-uris: Used for the installation of packages.
 # prediction-class: Used to provide custom prediction class names.
-
+PROJECT='ml-pipeline-309409'
 MODEL_NAME='test_iris'
 MODEL_VERSION='v1'
-RUNTIME_VERSION='1.14' 
+RUNTIME_VERSION='1.14'
 MODEL_CLASS='model_prediction.PyTorchIrisClassifier'
 PYTORCH_PACKAGE='gs://ml-pipeline-309409_bucket/packages/torch-1.8.1+cpu-cp37-cp37m-linux_x86_64.whl' #ignore for now
 DIST_PACKAGE='gs://ml-pipeline-309409_bucket/models/Test_model-0.1.tar.gz'
@@ -28,9 +28,11 @@ REGION='global'
 gcloud alpha ai-platform models create ${MODEL_NAME} \
 	--region=${REGION} \
        	--enable-logging \
-	--enable-console-logging
+	--enable-console-logging \
+	--project=${PROJECT}
 
 gcloud beta ai-platform versions create ${MODEL_VERSION} \
+		--project=${PROJECT}
     --model=${MODEL_NAME} \
     --origin=gs://${BUCKET_NAME}/${GCS_MODEL_DIR}/ \
     --python-version=3.5 \
