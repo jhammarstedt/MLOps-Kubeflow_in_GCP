61770b76f5828fc5b7442f89d5a40605e322d50e
jhammarstedt <johan.hammarstedt@gmail.com>
2021-05-06 16:11:50 +0200

    separate model file

--- containers/train/models/setup.py
+++ containers/train/models/setup.py
@@ -6,7 +6,7 @@ from setuptools import setup
 setup(
    name="Test_model",
    version="0.1",
-   scripts=["model_prediction.py","train_test.py"],
+   scripts=["model_prediction.py","network.py"],
    include_package_data=True,
    install_requires=['torch @ https://download.pytorch.org/whl/cpu/torch-0.4.1-cp35-cp35m-linux_x86_64.whl']
 
