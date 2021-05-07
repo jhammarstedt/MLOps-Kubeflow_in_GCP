d818220ac72820d6e1ff771616e8a01094145c28
Matej Sestak <matej.sestak@hotmail.com>
2021-05-07 13:43:54 +0200

    make cloud function

---
+++ containers/pipeline/deploy_cloudfunction.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+if [ "$#" -ne 1 ]; then
+    echo "Usage: ./deploy_cloudfunction.sh pipelines_host"
+    echo "  eg:  ./deploy_cloudfunction.sh 447cdd24f70c9541-dot-us-central1.notebooks.googleusercontent.com"
+    exit
+fi
+
+
+PROJECT=$(gcloud config get-value project)
+BUCKET="${PROJECT}_bucket"
+REGION=us-central1
+PIPELINES_HOST=$1
+
+
+gcloud functions deploy handle_newfile --runtime python37 \
+ --set-env-vars PROJECT=${PROJECT},BUCKET=${BUCKET},PIPELINES_HOST=${PIPELINES_HOST},HPARAM_JOB=${HPARAM_JOB} \
+ --trigger-resource="${BUCKET}/data"  \
+ --trigger-event=google.storage.object.finalize
